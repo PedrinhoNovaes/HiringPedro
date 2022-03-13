1671e2bdeea6e9c3ca53b54d85f14add37ce74e7
Pedro F R Novaes <pe.fer@hotmail.com>
2022-03-13 04:05:13 -0300

    awating deploy

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,21 @@
 import "./intro.scss";
-
+import {init} from 'ityped'
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+  
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Node JS", "React", "Git/Scrum", "TypeScript"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +27,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Pedro Novaes</h1>
-          <h3>Developer <span>Node</span></h3>
+          <h3>Developer <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/Arrow-down.png" alt="" />
